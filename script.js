7e87b83571b593c976a464167d0fe764ef9ec143
Ledjan Miçani <155323267+Choopin86@users.noreply.github.com>
2025-08-09 08:26:43 +0200

    Container of the grid completed

---
+++ script.js
@@ -0,0 +1,18 @@
+//Create a div container on the HTML file
+//query the container div
+//Create a 16X16 grid of square divs
+//Use flexbox to arrange the divs as a grid
+//Use CSS to style them
+
+const container = document.querySelector(".container");
+
+// function createGrid(number) {
+//   const numberOfCells = number * number;
+//   for (let i = 0; i < numberOfCells; i++) {
+//     document.createElement("div");
+//     div.setAttribute("class", "cell");
+//     return div;
+//   }
+// }
+
+// const gridSize = createGrid(16);
