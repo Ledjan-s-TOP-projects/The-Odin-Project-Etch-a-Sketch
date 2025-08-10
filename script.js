872baf2d6d5bd341e735869aa7f224d6d45126a0
Ledjan Miçani <155323267+Choopin86@users.noreply.github.com>
2025-08-10 17:01:18 +0200

    Layout completed with flex, Grid Size button implemented

--- script.js
+++ script.js
@@ -1,29 +1,56 @@
 const body = document.querySelector("body");
+const options = document.querySelector(".options");
 const container = document.querySelector(".container");
-const options = document.createElement("div");
-options.setAttribute("class", "options");
-body.appendChild(options);
 
 const gridSizeButton = document.createElement("button");
 options.appendChild(gridSizeButton);
 gridSizeButton.setAttribute("class", "btn");
 gridSizeButton.textContent = "Grid Size";
 
-let userInput = 16;
-createGrid(userInput);
+gridSizeButton.addEventListener("click", () => {
+  const userInput = prompt(
+    "Enter a number between 1-100 to set the grid size."
+  );
+  const newSize = parseInt(userInput, 10);
+  console.log(newSize);
 
-gridSizeButton.addEventListener("click", (e) => {
-  userInput = prompt("Enter a number between 1-100 to set the grid size.");
-  createGrid(userInput);
-  return userInput;
+  if (userInput === null) {
+    createGrid(); //When the user clicks cancel, the system will choose the default size
+  } else if (isNaN(newSize)) {
+    alert("Try again using only numbers between 1-100 to set the grid size.");
+  } else if (newSize < 0 || newSize > 101) {
+    alert("Try again! Enter a number between 1-100 to set the grid size");
+  } else if (!Number.isInteger(newSize)) {
+    alert("Try again using whole numbers between 1-100 without decimals.");
+  } else {
+    createGrid(newSize);
+  }
+
+  // if (newSize === null) {
+  //   createGrid();
+  // } else if (newSize <= 0 || newSize > 100) {
+  //   alert("Try again! Enter a number between 1-100 to set the grid size");
+  // } else if (isNaN(newSize)) {
+  //   alert("Try again! Enter only numbers between 1-100 to set the grid size");
+  // } else {
+  //   createGrid(newSize);
+  // }
 });
 
-function createGrid(userInput) {
-  const numberOfCells = userInput * userInput;
+function createGrid(size = 16) {
+  clearGrid();
+  const numberOfCells = size * size;
   for (let i = 0; i < numberOfCells; i++) {
     const cell = document.createElement("div");
     container.appendChild(cell);
     cell.setAttribute("class", "cell");
-    container.style.gridTemplateColumns = `repeat(${userInput}, 1fr)`;
+    cell.style.width = `${800 / size}px`;
+    cell.style.height = `${800 / size}px`;
   }
 }
+
+function clearGrid() {
+  container.innerHTML = "";
+}
+
+createGrid();
