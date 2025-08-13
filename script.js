6459addf59a9174236563070021247dbe727c184
Ledjan Miçani <155323267+Choopin86@users.noreply.github.com>
2025-08-13 12:08:31 +0200

    Implementing opacity for black color and drawing with random colors

--- script.js
+++ script.js
@@ -2,21 +2,39 @@ const body = document.querySelector("body");
 const options = document.querySelector(".options");
 const container = document.querySelector(".container");
 
-container.addEventListener("mouseover", (event) => {
-  if (event.target.classList.contains("cell")) {
-    event.target.style.backgroundColor = "black";
-  }
-});
-
+//------Buttons-------
 const gridSizeButton = document.createElement("button");
 options.appendChild(gridSizeButton);
 gridSizeButton.setAttribute("class", "btn");
+gridSizeButton.style.cursor = "pointer";
 gridSizeButton.textContent = "Grid Size";
 
 const clearButton = document.createElement("button");
 options.appendChild(clearButton);
+clearButton.style.cursor = "pointer";
 clearButton.textContent = "Clear";
 
+let currentMode = null;
+const colors = [
+  "blue",
+  "green",
+  "yellow",
+  "red",
+  "orange",
+  "pink",
+  "magenta",
+  "purple",
+];
+
+const blackButton = document.createElement("button");
+blackButton.setAttribute("class", "blackBtn");
+options.appendChild(blackButton);
+
+const randomColorButton = document.createElement("button");
+randomColorButton.setAttribute("class", "randomColorBtn");
+options.appendChild(randomColorButton);
+
+//------Event Listeners for Buttons-------
 gridSizeButton.addEventListener("click", () => {
   const userInput = prompt(
     "Enter a number between 1-100 to set the grid size."
@@ -28,7 +46,7 @@ gridSizeButton.addEventListener("click", () => {
     createGrid(); //When the user clicks cancel, the system will choose the default size
   } else if (isNaN(newSize)) {
     alert("Try again using only numbers between 1-100 to set the grid size.");
-  } else if (newSize < 0 || newSize > 101) {
+  } else if (newSize < 0 || newSize > 100) {
     alert("Try again! Enter a number between 1-100 to set the grid size");
   } else if (!Number.isInteger(newSize)) {
     alert("Try again using whole numbers between 1-100 without decimals.");
@@ -41,6 +59,35 @@ clearButton.addEventListener("click", () => {
   clear();
 });
 
+blackButton.addEventListener("click", () => {
+  currentMode = "black";
+});
+
+randomColorButton.addEventListener("click", () => {
+  currentMode = "random";
+});
+
+//------Event Listners for Drawing-------
+container.addEventListener("mouseover", (event) => {
+  if (event.target.classList.contains("cell")) {
+    let cell = event.target;
+    if (!cell.dataset.opacity) {
+      cell.dataset.opacity = 0;
+    }
+    if (currentMode === "black") {
+      cell.dataset.opacity = Math.min(
+        parseFloat(cell.dataset.opacity) + 0.1,
+        1
+      );
+      cell.style.backgroundColor = "black";
+      cell.style.opacity = cell.dataset.opacity;
+    } else if (currentMode === "random") {
+      const randomChoice = Math.floor(Math.random() * colors.length);
+      event.target.style.backgroundColor = colors[randomChoice];
+    }
+  }
+});
+//------Functions------
 function createGrid(size = 100) {
   clearGrid();
   const numberOfCells = size * size;
@@ -60,7 +107,9 @@ function clearGrid() {
 function clear() {
   for (const cell of container.children) {
     cell.style.backgroundColor = "white";
+    cell.dataset.opacity = 0;
   }
 }
 
+//----//Initial grid creation-------
 createGrid();
